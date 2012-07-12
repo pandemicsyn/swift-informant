26ca91b8fb3d20337d41f5b27418e10d746bae8d
Florian Hines <syn@ronin.io>
2012-07-12 16:25:57 -0500

    Bump version.

--- informant/__init__.py
+++ informant/__init__.py
@@ -2,7 +2,7 @@ import gettext
 
 
 #: Version information (major, minor, revision[, 'dev']).
-version_info = (0, 0, 7)
+version_info = (0, 0, 8)
 #: Version string 'major.minor.revision'.
 version = __version__ = ".".join(map(str, version_info))
 gettext.install('informant')
